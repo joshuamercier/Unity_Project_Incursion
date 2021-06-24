535e64acaa4f71661127b55551fb251ad708a2ed
Josh <joshuamercier@yahoo.com>
2021-06-23 20:55:07 -0400

    Background moves. Needs some tweaks to be more smooth.

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    // Class variables
+    public GameObject upperSpace;
+    public GameObject lowerSpace;
+
+    private Vector3 resetPos;
+    private Vector3 posWhenReset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Grab position to where we will reset the object to
+        resetPos = upperSpace.transform.position;
+        // Grab position to know when we should reset the object
+        posWhenReset = lowerSpace.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.z <= posWhenReset.z)
+        {
+            transform.position = resetPos;
+        }
     }
 }
