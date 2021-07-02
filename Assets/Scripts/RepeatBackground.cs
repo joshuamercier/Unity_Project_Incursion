ce9f83f2d203210143da8e8aa0ff08460cb21007
Josh <joshuamercier@yahoo.com>
2021-07-02 15:03:21 -0400

    Slight cleanup

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -9,6 +9,7 @@ public class RepeatBackground : MonoBehaviour
 
     private Material material;
     private Vector2 offset;
+
     // Start is called before the first frame update
     void Start()
     {
