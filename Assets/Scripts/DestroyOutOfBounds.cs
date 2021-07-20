bc2bb077e2edadc85442b71b60240bb618ee4e64
Josh <joshuamercier@yahoo.com>
2021-07-20 15:51:41 -0400

    Enemies and obstacles properly move now. Just spawn left to fix.

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,7 +6,8 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     // Class variables
     private float xRange = 70.0f; // Boundary of X-axis destruction
-    private float yRange = 40.0f; // boundary of player Z-axis
+    private float yUpperRange = 75.0f; // boundary of lower Y-axis destruction
+    private float yLowerRange = -15.0f; // boundary of lower Y-axis destruction
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,7 @@ public class DestroyOutOfBounds : MonoBehaviour
     void Update()
     {
         // If an object goes past the game boundaries, remove that object
-        if (transform.position.y > yRange || transform.position.y < -yRange || transform.position.x > xRange || transform.position.x < -xRange)
+        if (transform.position.y > yUpperRange || transform.position.y < yLowerRange || transform.position.x > xRange || transform.position.x < -xRange)
         {
             Destroy(gameObject);
         }
--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -15,6 +15,6 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.down * Time.deltaTime * speed);
     }
 }
